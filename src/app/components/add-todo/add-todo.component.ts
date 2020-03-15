ae0b0b15d9bb2e6f614ab9d8d46a3da67dd689e3
PawelRadawiec <pawel.radaw@gmail.com>
2020-03-15 09:46:03 +0100

    modal / reset form

--- src/app/components/add-todo/add-todo.component.ts
+++ src/app/components/add-todo/add-todo.component.ts
@@ -3,13 +3,12 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Todo, TodoFilter } from '../models/todo.model';
 import { select, Store } from '@ngrx/store';
 import { State } from '../../store/state/app.state';
-import { SearchRequest, TodoGetByIdResponse, CreateTodoRequest, CreateTodoResponse } from '../../store/todos/todos.actions';
+import { SearchRequest, CreateTodoRequest, CreateTodoResponse } from '../../store/todos/todos.actions';
 import { Subscription } from 'rxjs';
 import { selectGetByIdTodo, selectCreateResponse } from '../../store/selectors/todo.selector';
 import { AutoUnsubscribe } from 'ngx-auto-unsubscribe';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { ErrorComponent } from '../error/error.component';
-import { ActivatedRoute } from '@angular/router';
 import { Project } from '../models/project.model';
 import { selectProjectId } from 'src/app/store/selectors/project.selector';
 import * as todoActions  from '../../store/todos/todos.actions';
@@ -29,8 +28,7 @@ export class AddTodoComponent extends ErrorComponent implements OnInit, OnDestro
   constructor(
     private modalService: NgbModal,
     protected store: Store<State>,
-    private formBuilder: FormBuilder,
-    private route: ActivatedRoute
+    private formBuilder: FormBuilder
   ) {
     super(store);
     this.subscriptions.push(
@@ -68,8 +66,7 @@ export class AddTodoComponent extends ErrorComponent implements OnInit, OnDestro
   closeModal() {
     this.modalService.dismissAll();
     this.store.dispatch(new SearchRequest(new TodoFilter()));
-    this.store.dispatch(new TodoGetByIdResponse(new Todo()));
-    this.store.dispatch(new CreateTodoResponse(null));
+    this.todoForm.reset();
   }
 
   open() {
@@ -79,8 +76,7 @@ export class AddTodoComponent extends ErrorComponent implements OnInit, OnDestro
   initTodoForm() {
     this.todoForm = this.formBuilder.group({
       title: [],
-      description: [],
-      status: []
+      description: []
     });
   }
 }
